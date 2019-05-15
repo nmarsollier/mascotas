34499d304d5f3df891bc36d23dd24eb801b28642
nmarsollier <nmarsollier@gmail.com>
2019-05-15 11:11:52 -0300

    comillas

--- react/src/api/userApi.js
+++ react/src/api/userApi.js
@@ -1,34 +1,34 @@
-import axios from "axios"
+import axios from "axios";
 
-axios.defaults.headers.common['Content-Type'] = 'application/json'
-axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 export function getCurrentToken() {
-    return localStorage.getItem('token')
+    return localStorage.getItem("token");
 }
 
 function setCurrentToken(token) {
-    localStorage.setItem('token', token);
-    axios.defaults.headers.common['Authorization'] = "bearer " + token;
+    localStorage.setItem("token", token);
+    axios.defaults.headers.common["Authorization"] = "bearer " + token;
 }
 
 export function getCurrentUser() {
-    return localStorage.getItem('user')
+    return localStorage.getItem("user");
 }
 
 export function logout() {
-    localStorage.removeItem('token')
-    localStorage.removeItem('user')
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
     return new Promise(function (resolve, reject) {
         axios.get("http://localhost:3000/v1/user/signout")
             .then(res => {
-                resolve(res.data)
+                resolve(res.data);
             })
             .catch(err => {
-                console.log(err)
-                reject(err)
-            })
-    })
+                console.log(err);
+                reject(err);
+            });
+    });
 }
 
 export function login(payload) {
@@ -36,14 +36,14 @@ export function login(payload) {
         axios.post("http://localhost:3000/v1/user/signin",
             payload)
             .then(res => {
-                setCurrentToken(res.data.token)
-                resolve(res.data)
+                setCurrentToken(res.data.token);
+                resolve(res.data);
             })
             .catch(err => {
-                console.log(err)
-                reject(err)
-            })
-    })
+                console.log(err);
+                reject(err);
+            });
+    });
 }
 
 export function reloadCurrentUser() {
@@ -51,15 +51,15 @@ export function reloadCurrentUser() {
         if (getCurrentToken() !== undefined) {
             axios.get("http://localhost:3000/v1/users/current")
                 .then(res => {
-                    localStorage.setItem('user', res.data);
-                    resolve(res.data)
+                    localStorage.setItem("user", res.data);
+                    resolve(res.data);
                 })
                 .catch(err => {
-                    console.log(err)
-                    reject(err)
-                })
+                    console.log(err);
+                    reject(err);
+                });
         }
-    })
+    });
 }
 
 export function newUser(payload) {
@@ -67,15 +67,15 @@ export function newUser(payload) {
         if (getCurrentToken() !== undefined) {
             axios.post("http://localhost:3000/v1/user", payload)
                 .then(res => {
-                    setCurrentToken(res.data.token)
-                    resolve(res.data)
+                    setCurrentToken(res.data.token);
+                    resolve(res.data);
                 })
                 .catch(err => {
-                    console.log(err)
-                    reject(err)
-                })
+                    console.log(err);
+                    reject(err);
+                });
         }
-    })
+    });
 }
 
 export function changePassword(payload) {
@@ -83,16 +83,16 @@ export function changePassword(payload) {
         if (getCurrentToken() !== undefined) {
             axios.post("http://localhost:3000/v1/user/password", payload)
                 .then(res => {
-                    resolve(res.data)
+                    resolve(res.data);
                 })
                 .catch(err => {
-                    console.log(err)
-                    reject(err)
-                })
+                    console.log(err);
+                    reject(err);
+                });
         }
-    })
+    });
 }
 
 if (getCurrentToken() !== undefined) {
-    setCurrentToken(getCurrentToken())
+    setCurrentToken(getCurrentToken());
 }
